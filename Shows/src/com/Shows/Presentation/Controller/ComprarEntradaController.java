8c11b8090f645b59d65421cf660b6f00dba970c4
Khristian <gkhristian@gmail.com>
2012-06-09 22:02:42 +0200

    Presentation --> ComprarEntredaController headers ON!

--- Shows/src/com/Shows/Presentation/Controller/ComprarEntradaController.java
+++ Shows/src/com/Shows/Presentation/Controller/ComprarEntradaController.java
@@ -1,11 +1,42 @@
 package com.Shows.Presentation.Controller;
 
+import java.util.Set;
+
+import javax.xml.crypto.Data;
+
 import com.Shows.Domain.Controllers.ComprarEntradaUseCaseController;
+import com.Shows.TupleTypes.PosicioSeient;
 
 public class ComprarEntradaController {
 	private ComprarEntradaUseCaseController comprarEntradaUseCaseController;
 
 	public ComprarEntradaController() {
 		comprarEntradaUseCaseController = new ComprarEntradaUseCaseController();
+	}
+	
+	public void PrOkObteRepresentacions(String titol, Data date) {
+	}
+
+	public void PrOkObteOcupacio(String numLocal, String sessio, int nombEspectadors) {
+	}
+	
+	public void PrOkSelecionarSeients(Set<PosicioSeient> seients) {
+	}
+	
+	public void PrOkPagament(String dni, int codiB, String numCompte) {
+	}
+	
+	public void PrCancellar() {
+	}
+	
+	public void PrCancellarAvis() {
+	}
+	
+	public void PrFi() {
+		
+	}
+	
+	public void canviPreuMoneda(String moneda) {
+		//TODO esto esta bien???
 	}
 }
