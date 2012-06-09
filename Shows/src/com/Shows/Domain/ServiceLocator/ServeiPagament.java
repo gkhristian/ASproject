05c453dad7c4029bae50e54a6de7678627a2cf45
Khristian <gkhristian@gmail.com>
2012-06-09 18:05:36 +0200

    Adapters pagament + Servicelocator

---
+++ Shows/src/com/Shows/Domain/ServiceLocator/ServeiPagament.java
@@ -0,0 +1,12 @@
+package com.Shows.Domain.ServiceLocator;
+
+import java.util.Date;
+
+public class ServeiPagament {
+
+    public boolean autoritza(String numTarj, Date dCad, float preuTotal) {
+        
+        return true;
+    }
+    
+}
